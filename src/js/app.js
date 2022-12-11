3032260a5fd0bdd5bf8b9808bcbf356f96eb0827
Ewa Olczak <ewa.olczak@gmail.com>
2022-12-11 17:15:35 +0100

    Add Finder but it has an error

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
+import { select } from './settings.js';
+import Finder from './components/Finder.js';
+
 const app = {
-  init: function(){},
+  // eslint-disable-next-line no-unused-vars
+  initFinder: function (element) {
+    const thisApp = this;
+
+    const finderElement = document.querySelector(select.containerOf.finder);
+    thisApp.finder = new Finder(finderElement);
+  },
+
+  init: function () {
+    // eslint-disable-next-line no-undef
+    this.initFinder(element);
+  },
 };
 
 app.init();
---
+++ src/js/components/Finder.js
@@ -0,0 +1,41 @@
+import {templates} from '../settings.js';
+
+class Finder {
+  constructor(element) {
+    const thisFinder = this;
+
+    // save reference to finder page div
+    thisFinder.element = element;
+
+    // start at step 1
+    thisFinder.step = 1;
+
+    // render view for the first time
+    thisFinder.render();
+  }
+
+  render() {
+    const thisFinder = this;
+
+    // determine what title and button content should be used
+    // eslint-disable-next-line no-unused-vars
+    let pageData = null;
+    switch (thisFinder.step) {
+    case 1:
+      pageData = { title: 'Draw routes', buttonText: 'Finish drawing' };
+      break;
+    case 2:
+      pageData = { title: 'Pick start and finish', buttonText: 'Compute' };
+      break;
+    case 3:
+      pageData = { title: 'The best route is', buttonText: 'Start again' };
+      break;
+    }
+
+    // generate view from the template and set it as page content
+    const generatedHTML = templates.Finder(thisFinder.pageData);
+    thisFinder.element.innerHTML = generatedHTML;
+  }
+}
+
+export default Finder;
